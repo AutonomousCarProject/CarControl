c492b33838dccc45eabeec92fe92cffb9962c3d0
Vikram Kashyap <vk@vikramkashyap.com>
2018-07-16 14:11:36 -0700

    Added ImageManager

---
+++ ImageManagement/ImageManager.java
@@ -0,0 +1,15 @@
+/*ImageManager: Retrieves and preprocesses images from the camera and displays feed onscreen*/
+
+public class ImageManager {
+
+	/*Main*/
+	public ImageManager() {
+		
+	}
+
+	/*Serves monochrome raster of camera feed
+	 * Formatted in 1D array of bytes*/
+	public byte[] getMonochromeRaster() {
+		
+	}
+}
