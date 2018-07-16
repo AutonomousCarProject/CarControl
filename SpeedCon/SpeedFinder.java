c4d4ff52a35689dd40e5a950f5882350795747b2
derekaloha <derekalohahigh@gmail.com>
2018-07-16 13:42:56 -0700

    Finder Again

--- SpeedCon/SpeedFinder.java
+++ SpeedCon/SpeedFinder.java
@@ -1,5 +1,19 @@
 package SpeedCon;
 
+import drivedemo.DrDemo;
+
 public class SpeedFinder {
 
+	
+	public  double currentSpeed;
+	
+	
+	//It's the bad way, but it's a way
+	void calculateSpeed(int gasAmount)
+	{
+		currentSpeed = gasAmount;
+	}
+	
+	//gfdg
+	//void calculateSpeed(double )
 }
