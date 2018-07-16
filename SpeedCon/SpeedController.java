a585cc63ac57e0e01a54c42adc6cfe3aabcea343
William Adriance <williamadriance@Williams-MacBook-Pro-6.local>
2018-07-16 13:24:03 -0700

    basic speed controller

--- SpeedCon/SpeedController.java
+++ SpeedCon/SpeedController.java
@@ -1,5 +1,28 @@
 package SpeedCon;
-//comment
+
 public class SpeedController {
+	
+	private final double MAX_SPEED = 20;
+	private final double MIN_SPEED = 1;
+	
+	private double currentEstimatedSpeed;
+	private double desiredSpeed;
+	
+	public void calculateDesiredSpeed(){
+		double curveSteepness = 0; // Steering.getCurveSteepness();
+		desiredSpeed = Math.max((1 - curveSteepness)*MAX_SPEED, MIN_SPEED);
+	}
+	
+	public double getEstimatedSpeed(){
+		return currentEstimatedSpeed; 
+	}
+	
+	public void setEstimatedSpeed(double estimatedSpeed){
+		currentEstimatedSpeed = estimatedSpeed;
+	}
+	
+	public int getDesiredSpeed(){
+		return (int)desiredSpeed;
+	}
 	
 }
