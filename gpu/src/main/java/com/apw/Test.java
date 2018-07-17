5ce492a27f9b30805a91d320ee9df4faf4b58f0a
Rewind <sploing1@gmail.com>
2018-07-17 10:55:54 -0700

    Switched from JavaCL to AparAPI, added as dependency top pom.xml

--- gpu/src/main/java/com/apw/Test.java
+++ gpu/src/main/java/com/apw/Test.java
@@ -1,33 +1,25 @@
 package com.apw;
 
-import com.nativelibs4java.opencl.*;
-import com.nativelibs4java.opencl.CLMem.Usage;
-import org.bridj.Pointer;
-import java.io.IOException;
+import com.aparapi.Kernel;
+import com.aparapi.Range;
 
 public class Test {
-    public static void main(String[] args) throws IOException {
-        CLContext context = JavaCL.createBestContext();
-        CLQueue queue = context.createDefaultQueue();
+    public static void main(String[] args) {
 
-        int n = 1024;
-        
-        // Create OpenCL input and output buffers
-        CLBuffer<Float> 
-            a = context.createFloatBuffer(Usage.InputOutput, n), // a and b and read AND written to
-            b = context.createFloatBuffer(Usage.InputOutput, n),
-            out = context.createFloatBuffer(Usage.Output, n);
+        final float inA[] = { 1.0f, 2.0f, 3.0f };
+        final float inB[] = { 3.0f, 2.0f, 1.0f };
+        assert (inA.length == inB.length);
+        final float[] result = new float[inA.length];
 
-        TutorialKernels kernels = new TutorialKernels(context);
-        int[] globalSizes = new int[] { n };
-        CLEvent fillEvt = kernels.fill_in_values(queue, a, b, n, globalSizes, null);
-        CLEvent addEvt = kernels.add_floats(queue, a, b, out, n, globalSizes, null, fillEvt);
-        
-        Pointer<Float> outPtr = out.read(queue, addEvt); // blocks until add_floats finished
+        Kernel kernel = new Kernel() {
+            @Override
+            public void run() {
+                int i = getGlobalId();
+                result[i] = inA[i] + inB[i];
+            }
+        };
 
-        // Print the first 10 output values :
-        for (int i = 0; i < 10 && i < n; i++)
-            System.out.println("out[" + i + "] = " + outPtr.get(i));
-        
+        Range range = Range.create(result.length);
+        kernel.execute(range);
     }
 }
