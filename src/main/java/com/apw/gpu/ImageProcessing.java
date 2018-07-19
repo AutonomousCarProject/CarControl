b94d7969e9080552b1c871e1046897a8f8e55a06
Rewind <sploing1@gmail.com>
2018-07-19 10:23:16 -0700

    Added kernel test with dummy data

--- src/main/java/com/apw/gpu/ImageProcessing.java
+++ src/main/java/com/apw/gpu/ImageProcessing.java
@@ -1,9 +1,24 @@
 package com.apw.gpu;
 
+import com.aparapi.Range;
+
+import java.util.Arrays;
+
 public class ImageProcessing {
     public static void main(String[] args) {
 
-        System.out.println("rip KernelManager 2 days ago-today");
+        byte[] bayer = { 1, 2, 3, 4 };
+        byte[] simple = { 4, 3, 2, 1 };
+        int nrows = 4;
+        int ncols = 4;
+
+        Range range = Range.create(nrows, ncols);
+
+        SimpleColorRasterKernel kernel = new SimpleColorRasterKernel(nrows, ncols, bayer, simple);
+        kernel.execute(range);
+
+        System.out.println(Arrays.toString(kernel.getSimple()));
+
 
     }
 }
