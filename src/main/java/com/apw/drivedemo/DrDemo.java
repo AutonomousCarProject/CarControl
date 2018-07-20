399033645487924fd54476f597ab5e4f687eb16b
Matthew Alexander <mdalexand1@gmail.com>
2018-07-20 09:03:38 -0700

    Merge conflicts

--- src/main/java/com/apw/drivedemo/DrDemo.java
+++ src/main/java/com/apw/drivedemo/DrDemo.java
@@ -606,7 +606,6 @@ public class DrDemo extends JFrame implements MouseListener,KeyListener {
         ///** If you have self-driving code, you could put it here **///
         	//TestServos(); // (replace this with your own code)
         
-        
         //Begin Speed Code
         
         speedControl.onUpdate(this.GasPedal, testSteering.getDegreeOffset(), this.manualSpeed, graf);
