ffdce9738d83506132bb015b5a715759b63a733c
William Adriance <williamadriance@Williams-MacBook-Pro-6.local>
2018-07-23 12:08:48 -0700

    painting rects

--- src/main/java/com/apw/SpeedCon/SpeedController.java
+++ src/main/java/com/apw/SpeedCon/SpeedController.java
@@ -75,7 +75,14 @@ public class SpeedController {
 				else if (i.color.getColor() == Color.BLUE) {
 					graf.setColor(java.awt.Color.BLUE);
 				}
-				graf.drawRect(i.x+8, i.y+40, i.width, i.height);;
+				for(MovingBlob b:blobs){
+					int velocity = (int)(100*Math.sqrt(b.velocityX*b.velocityX + b.velocityY*b.velocityY));
+					int color = (velocity << 16) + (velocity << 8) + velocity;
+					this.trackSim.DrawLine(color, b.y, b.x, b.y+b.height, b.x);
+					this.trackSim.DrawLine(color, b.y, b.x, b.y, b.x+b.width);
+					this.trackSim.DrawLine(color, b.y+b.height, b.x, b.y+b.height, b.x+b.width);
+					this.trackSim.DrawLine(color, b.y, b.x+b.width, b.y+b.height, b.x+b.width);
+				}
 			}
 			if(detectRedLight(i)){
 				setStoppingAtLight();
