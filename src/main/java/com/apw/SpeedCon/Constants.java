f4388c21a20ddbf7e7e80ac9808b3651507e27e5
Matthew Alexander <mdalexand1@gmail.com>
2018-07-24 11:00:14 -0700

    Commented code, cleaned up code, and changed detection boundry overlay to track sim graphics

--- src/main/java/com/apw/SpeedCon/Constants.java
+++ src/main/java/com/apw/SpeedCon/Constants.java
@@ -2,20 +2,20 @@ package com.apw.SpeedCon;
 
 public class Constants {
 		public static final boolean
-		
+		//Default Debug Overlays
 		DEFAULT_OVERLAY = false,
 		DEFAULT_BLOBS = false,
 		DEFAULT_WRITE_BLOBS_TO_CONSOLE = false;
 	
 		public static final double 
-		//Speed
+		//Speed Related Constants
 		MAX_SPEED = 20,					//Car's maximum speed
 		MIN_SPEED = 10,					//Car's minimum speed
 		PIN_TO_METER_PER_SECOND = 0.4,	//Conversion for motor position to m/s. DO NOT TOUCH
 		MAX_OBJECT_WIDTH = 320,			//Maximum width in pixels of object height before car slows
 		MAX_OBJECT_HEIGHT = 200,		//Maximum height of in pixels of an object before car slows
 		
-		//Math
+		//Math Constants
 		GRAV = 9.80665,					//Average gravitational acceleration
 		SQRT_GRAV = Math.sqrt(GRAV),	//The square root of the average gravitational acceleration
 		FRICT = 0.75,					//Given coefficient of friction
@@ -28,18 +28,18 @@ public class Constants {
 		STOPSIGN_DRIFT_SPEED = 12,		//Speed at which the car pulls up to a stopsign
 		DRIFT_TO_STOPLIGHT_FRAMES = 0,	//Frames to drift forward after stoplight detected
 		STOPLIGHT_DRIFT_SPEED = 12,		//Speed at which the car pulls up to a stoplight
-		WAIT_AFTER_STOPSIGN = 100,
+		WAIT_AFTER_STOPSIGN = 100,		//Time between finding a stopsign and finding a stopsign again
 		
-		//Blobs
+		//Blob Filters
 		BLOB_HEIGHT = 5,				//Filtered height of a blob in pixels
 		BLOB_WIDTH = 5,					//Filtered width of a blob in pixels
-		BLOB_AGE = -1,					//Filtered age of a blob in frames
+		BLOB_AGE = 3,					//Filtered age of a blob in frames
 		STOPLIGHT_MIN_Y = 0,			//Filtered position of a blob in pixels
 		STOPLIGHT_MAX_Y = 240,			//Filtered position of a blob in pixels
-		STOPLIGHT_MIN_X = 8,			//Filtered position of a blob in pixels
-		STOPLIGHT_MAX_X = 630,			//Filtered position of a blob in pixels
-		STOPSIGN_MIN_Y = 32,			//Filtered position of a blob in pixels
-		STOPSIGN_MAX_Y = 512,			//Filtered position of a blob in pixels
+		STOPLIGHT_MIN_X = 0,			//Filtered position of a blob in pixels
+		STOPLIGHT_MAX_X = 640,			//Filtered position of a blob in pixels
+		STOPSIGN_MIN_Y = 0,				//Filtered position of a blob in pixels
+		STOPSIGN_MAX_Y = 480,			//Filtered position of a blob in pixels
 		STOPSIGN_MIN_X = 480,			//Filtered position of a blob in pixels
-		STOPSIGN_MAX_X = 648;			//Filtered position of a blob in pixels
+		STOPSIGN_MAX_X = 640;			//Filtered position of a blob in pixels
 }
