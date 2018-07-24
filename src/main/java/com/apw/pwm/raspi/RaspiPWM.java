1a1add2296a0a3aa8b6b3d17e3e315dfef5c38d3
edelmanjm <edelmanjm@users.noreply.github.com>
2018-07-24 03:45:42 -0700

    Add basic outline
    It's not the actual code, but it's pretty close to it structure-wise

--- src/main/java/com/apw/pwm/raspi/RaspiPWM.java
+++ src/main/java/com/apw/pwm/raspi/RaspiPWM.java
@@ -1,10 +1,15 @@
 package com.apw.pwm.raspi;
 
 import com.apw.pwm.PWMController;
+import com.pi4j.io.gpio.GpioController;
+import com.pi4j.io.gpio.GpioFactory;
+import com.pi4j.io.gpio.RaspiPin;
 
 public class RaspiPWM implements PWMController {
 
   private static final RaspiPWM theController = new RaspiPWM();
+
+  private final GpioController gpioController = GpioFactory.getInstance();
 
   private RaspiPWM() {
 
@@ -17,8 +22,10 @@ public class RaspiPWM implements PWMController {
 
   @Override
   public boolean IsOpen() {
-    return false;
+    return gpioController.isShutdown();
   }
+
+  // TODO wtf are the mode and value bytes actually representing
 
   @Override
   public void pinMode(int pin, byte mode) {
@@ -27,21 +34,23 @@ public class RaspiPWM implements PWMController {
 
   @Override
   public void digitalWrite(int pin, byte value) {
-
+    // TODO real implementation
+    gpioController.provisionDigitalOutputPin(RaspiPin.getPinByAddress(pin)).pulse(value);
   }
 
   @Override
   public void servoWrite(int pin, int angle) {
-
+    // TODO real implementation
+    gpioController.provisionDigitalOutputPin(RaspiPin.getPinByAddress(pin)).pulse(angle);
   }
 
   @Override
   public void Open() {
-
+    // TODO figure out why this is even necessary instead of just being called in the constructor
   }
 
   @Override
   public void Close() {
-
+    gpioController.shutdown();
   }
 }
