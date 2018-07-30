98b063b6d5afe3b8ba4e0a11042a54a2f59d1bde
derekaloha <derekalohahigh@gmail.com>
2018-07-30 10:33:33 -0700

    fix test width

--- src/main/java/com/apw/speedcon/CameraCalibration.java
+++ src/main/java/com/apw/speedcon/CameraCalibration.java
@@ -41,7 +41,7 @@ public class CameraCalibration {
     private double signWidth;           //The width of a standared stop sign in mm
 
     private MovingBlob testBlob;
-    private double testBlobWidthHeight = 10; //the width and height of a square used to calibrate the camera
+    private double testBlobWidthHeight = 9; //the width and height of a square used to calibrate the camera
     private double testBlobDistance = 20;    //The distance the test blob is away from the camera
     private double relativeWorldScale;  //The scale of the world (if 1/3 scale, set to 3)
 
