dc5297b004db0a5e2b7e6bb840f68c4c3b72cd4a
Coosome Joe <thecoltonjelsema@gmail.com>
2018-08-02 09:16:04 -0700

    Time logging
    Now logs the ammount of time past the requested timing the PWM actions are. To be used for testing and eventually fixing the jitter.

--- ArduinoCode/ArduinoCode.ino
+++ ArduinoCode/ArduinoCode.ino
@@ -15,6 +15,8 @@ byte type;
 byte pin;
 byte value;
 
+unsigned long overtime = 20;
+
 unsigned long lastNoKill = 0;
 unsigned long sinceConnect = 0;
 unsigned long sinceNoKill = 0; //Input timing for kill
@@ -141,15 +143,30 @@ void loop() {
     lastTime = micros();
   }
 
+  int timing = micros()-lastTime;
   //Turn off the signal at approximately the correct timing.
-  if (steerON && micros()-lastTime >= steerDelay){
+  if (steerON && timing >= steerDelay){
     digitalWrite(9, LOW);
     steerON = false;
+
+    if (timing - steerDelay > overtime) {
+      overtime = timing - steerDelay;
+      if (overtime < 255){
+        addMessage(8, 0, overtime);
+      } else {addMessage(9, 0, overtime);}
+    }
   }
 
-  if (wheelON && micros()-lastTime >= wheelDelay){
+  if (wheelON && timing >= wheelDelay){
     digitalWrite(10, LOW);
     wheelON = false;
+    
+    if (timing - wheelDelay > overtime) {
+      overtime = timing - wheelDelay;
+      if (overtime < 255){
+        addMessage(8, 1, overtime);
+      } else {addMessage(9, 1, overtime);}
+    }
   }
   
   if (!kill && !timedout){
