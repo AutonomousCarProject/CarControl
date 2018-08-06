042bea6b68f9e92e62ededa5e81bfada5e5d4947
Rewind <sploing1@gmail.com>
2018-08-06 12:29:39 -0700

    Added BlackWhiteRasterKernel and BlackWhiteRaster2Kernel
    Removed ImageKernels

--- src/main/java/com/apw/gpu/BlackWhiteRaster2Kernel.java
+++ src/main/java/com/apw/gpu/BlackWhiteRaster2Kernel.java
@@ -3,10 +3,10 @@ package com.apw.gpu;
 import com.aparapi.Kernel;
 
 /**
- * The <code>BlackWhiteRasterKernel</code> subclass describes a {@link com.aparapi.Kernel Kernel}
+ * The <code>BlackWhiteRaster2Kernel</code> subclass describes a {@link com.aparapi.Kernel Kernel}
  * that converts a bayer rgb byte array into a black and white bayer byte array.
  */
-public class BlackWhiteRasterKernel extends Kernel {
+public class BlackWhiteRaster2Kernel extends Kernel {
 
     private int nrows, ncols;
 
@@ -16,17 +16,15 @@ public class BlackWhiteRasterKernel extends Kernel {
 
     private int averageLuminance;
 
-    private int luminanceMultiplier = 1;
-
     /**
-     * Constructs an <code>BlackWhiteRasterKernel</code> Aparapi {@link com.aparapi.opencl.OpenCL OpenCL} kernel.
+     * Constructs an <code>BlackWhiteRaster2Kernel</code> Aparapi {@link com.aparapi.opencl.OpenCL OpenCL} kernel.
      *
      * @param bayer Array of bayer arranged rgb colors
      * @param mono  Monochrome copy of the bayer array
      * @param nrows Number of rows to filter
      * @param ncols Number of columns to filter
      */
-    public BlackWhiteRasterKernel(byte[] bayer, byte[] mono, int nrows, int ncols, byte tile) {
+    public BlackWhiteRaster2Kernel(byte[] bayer, byte[] mono, int nrows, int ncols, byte tile) {
         this.bayer = bayer;
         this.mono = mono;
         this.nrows = nrows;
@@ -34,10 +32,10 @@ public class BlackWhiteRasterKernel extends Kernel {
         this.tile = tile;
     }
 
-    public BlackWhiteRasterKernel() {}
+    public BlackWhiteRaster2Kernel() {}
 
     /**
-     * Sets all member variables of <code>BlackWhiteRasterKernel</code>.
+     * Sets all member variables of <code>BlackWhiteRaster2Kernel</code>.
      *
      * @param bayer Array of bayer arranged rgb colors
      * @param mono  Monochrome copy of the bayer array
@@ -64,41 +62,29 @@ public class BlackWhiteRasterKernel extends Kernel {
 
     @Override
     public void run() {
+        System.out.println("BlackWhite2RasterKernel.run");
         int row = getGlobalId(0);
         int col = getGlobalId(1);
+
         if (col == 0)
             averageLuminance = 0;
         int R = (bayer[getPos(col, row, combineTile((byte) 0, tile), ncols, nrows)] & 0xFF);
         int G = (bayer[getPos(col, row, combineTile((byte) 1, tile), ncols, nrows)] & 0xFF);
         int B = (bayer[getPos(col, row, combineTile((byte) 3, tile), ncols, nrows)] & 0xFF);
-        averageLuminance += (R + G + B) / 3;
 
         if (col == 0)
-            averageLuminance /= ncols;
+            averageLuminance = (R + G + B) / 3;
 
-        if (col < ncols) {
-            int R1 = (bayer[getPos(col, row, combineTile((byte) 0, tile), ncols, nrows)] & 0xFF);
-            int G1 = (bayer[getPos(col, row, combineTile((byte) 1, tile), ncols, nrows)] & 0xFF);
-            int B1 = (bayer[getPos(col, row, combineTile((byte) 3, tile), ncols, nrows)] & 0xFF);
-            int R2 = (bayer[getPos(col + 1, row, combineTile((byte) 0, tile), ncols, nrows)] & 0xFF);
-            int G2 = (bayer[getPos(col + 1, row, combineTile((byte) 1, tile), ncols, nrows)] & 0xFF);
-            int B2 = (bayer[getPos(col + 1, row, combineTile((byte) 3, tile), ncols, nrows)] & 0xFF);
-            int R3 = (bayer[getPos(col + 2, row, combineTile((byte) 0, tile), ncols, nrows)] & 0xFF);
-            int G3 = (bayer[getPos(col + 2, row, combineTile((byte) 1, tile), ncols, nrows)] & 0xFF);
-            int B3 = (bayer[getPos(col + 2, row, combineTile((byte) 3, tile), ncols, nrows)] & 0xFF);
-
-            int pix = (R1 + R2 + R3 + B1 + B2 + B3 + G1 + G2 + G3) / 9;
-            // int pix = (R2 + G2 + B2)/3;
-            if (!(col + 1 >= 640 || row < 240 || row > 455)) {
-                if (pix > luminanceMultiplier * averageLuminance) {
-                    mono[row * ncols + col + 1] = 1;
-                } else {
-                    mono[row * ncols + col + 1] = 0;
-                }
+        if (!(col >= 640 || row < 240 || row > 455)) {
+            if ((averageLuminance + (R + G + B) / 3) / 2 > averageLuminance * 1.5) {
+                mono[row * ncols + col] = 1;
             } else {
-                mono[row * ncols + col + 1] = 0;
+                mono[row * ncols + col] = 0;
             }
+        } else {
+            mono[row * ncols + col] = 0;
         }
+        averageLuminance = (averageLuminance + (R + G + B) / 3) / 2;
     }
 
     private int getBit(byte tile, int pos) {
