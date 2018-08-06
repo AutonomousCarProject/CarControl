35bb264df7abc1029ba3afad78ee2bed276907ba
Rewind <sploing1@gmail.com>
2018-08-06 12:45:02 -0700

    Added RGBRasterKernel, MonochromeRasterKernel, and MonoToRGBKernel

--- src/main/java/com/apw/gpu/MonochromeRasterKernel.java
+++ src/main/java/com/apw/gpu/MonochromeRasterKernel.java
@@ -12,6 +12,8 @@ public class MonochromeRasterKernel extends Kernel {
 
     private byte[] bayer, mono;
 
+    private byte tile;
+
     /**
      * Constructs an <code>MonochromeRasterKernel</code> Aparapi {@link com.aparapi.opencl.OpenCL OpenCL} kernel.
      *
@@ -20,12 +22,15 @@ public class MonochromeRasterKernel extends Kernel {
      * @param nrows Number of rows to filter
      * @param ncols Number of columns to filter
      */
-    public MonochromeRasterKernel(byte[] bayer, byte[] mono, int nrows, int ncols) {
+    public MonochromeRasterKernel(byte[] bayer, byte[] mono, int nrows, int ncols, byte tile) {
         this.bayer = bayer;
         this.mono = mono;
         this.nrows = nrows;
         this.ncols = ncols;
+        this.tile = tile;
     }
+
+    public MonochromeRasterKernel() {}
 
     /**
      * Sets all member variables of <code>MonochromeRasterKernel</code>.
@@ -35,11 +40,12 @@ public class MonochromeRasterKernel extends Kernel {
      * @param nrows Number of rows to filter
      * @param ncols Number of columns to filter
      */
-    public void setValues(byte[] bayer, byte[] mono, int nrows, int ncols) {
+    public void setValues(byte[] bayer, byte[] mono, int nrows, int ncols, byte tile) {
         this.bayer = bayer;
         this.mono = mono;
         this.nrows = nrows;
         this.ncols = ncols;
+        this.tile = tile;
     }
 
     /**
@@ -55,9 +61,9 @@ public class MonochromeRasterKernel extends Kernel {
     @Override
     public void run() {
 
-        int rows = getGlobalId(0);
-        int cols = getGlobalId(1);
+        int row = getGlobalId(0);
+        int col = getGlobalId(1);
 
-        mono[rows * ncols + cols] = (byte) ((((int) bayer[(rows * ncols * 2 + cols) * 2 + 1]) & 0xFF)); //Use only top right (green)
+        mono[row * ncols + col] = (byte) ((((int) bayer[(row * ncols * 2 + col) * 2 + 1]) & 0xFF));
     }
 }
