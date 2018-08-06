4c7b3bcc597ba39e9543f592314d784537a72997
Sameer Suri <sameer.suri0@gmail.com>
2018-08-06 10:42:08 -0700

    Removed outdated/redundant interfaces to make code easier to read/write/maintain

--- src/main/java/com/apw/pedestrians/image/IPixel.java
+++
@@ -1,8 +0,0 @@
-package com.apw.pedestrians.image;
-
-public interface IPixel {
-    // 0 = red, 1 = green, 2 = blue, 3 = gray, 4 = black, 5 = white
-    Color getColor();
-
-    void setColor(Color c);
-}
