ff00382a7ab10360f49795bf290cfe164d62de19
Matthew Alexander <mdalexand1@gmail.com>
2018-08-07 14:59:15 -0700

    Notes

--- src/main/java/com/apw/speedcon/SpeedControlModule.java
+++ src/main/java/com/apw/speedcon/SpeedControlModule.java
@@ -373,11 +373,7 @@ public class SpeedControlModule implements Module {
 			stopsignWaitFirst();
 			
 			double blobRealSize = getStopReal(stoppingBlob); //Gets real size
-<<<<<<< HEAD
-			distToBlob = cameraCalibrator.distanceToObj(blobRealSize/cameraCalibrator.relativeWorldScale, stoppingBlob.width, sizeCons.SIGN_INFO.get(stoppingBlob.type).get(6)); //Finds distance to closest blob based on real wrold size and pixel size
-=======
 			distToBlob = cameraCalibrator.distanceToObj(blobRealSize, stoppingBlob.width, sizeCons.SIGN_INFO.get(stoppingBlob.type).get(6)); //Finds distance to closest blob based on real wrold size and pixel size
->>>>>>> fea3f03586da54b4c62276deea2f0a2dc1b4af03
 			
 			System.out.println("frameWait: " + frameWait);
 			System.out.println("stopType: " + stopType);
@@ -404,7 +400,7 @@ public class SpeedControlModule implements Module {
 			stopsignWaitSubsequent();
 			
 			//distToBlob -= (rpmSpeed / Constants.WHEEL_GEARING) * Constants.WHEEL_CIRCUMFERENCE * Constant.TIME_DIFFERENCE;
-			distToBlob -= getEstimatedSpeed() * (Constant.TIME_DIFFERENCE / 1000);
+			distToBlob -= getEstimatedSpeed() * (Constant.TIME_DIFFERENCE / 1000.0);
 			
 			//System.out.println("frameWait: " + frameWait);
 			//System.out.println("stopType: " + stopType);
