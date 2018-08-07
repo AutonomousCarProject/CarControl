4eef0403afd231fe86efc599f7c6a825fdcd3fba
Vikram Kashyap <vkashyap737@gmail.com>
2018-08-07 13:54:24 -0700

    Modfied some settings. Does not have blob detection

--- src/main/java/com/apw/steering/SteeringMk2.java
+++ src/main/java/com/apw/steering/SteeringMk2.java
@@ -16,10 +16,10 @@ import java.util.ArrayList;
  */
 public class SteeringMk2 extends SteeringBase {
 
-    private final int NUM_PREVIOUS = 3;
-    private final int MAX_DIFF = 5;
-    private final double MIN_DIST_LOOK = 0.8;
-    private final double MAX_DIST_LOOK = 0.9;
+    private final int NUM_PREVIOUS = 4;
+    private final int MAX_DIFF = 7;
+    private final double MIN_DIST_LOOK = 0.5;
+    private final double MAX_DIST_LOOK = 0.8;
     private final double LINE_TRANSLATION_MULTIPLIER = 0.4;
 
     private boolean leftSideFound = false;
@@ -152,7 +152,7 @@ public class SteeringMk2 extends SteeringBase {
                         midX = lastX - MAX_DIFF;
                     }
                 }
-                //midPoints.add(new Point(midX, cameraRow));
+                midPoints.add(new Point(midX, cameraRow));
             }
 
             rightSideFound = false;
@@ -182,7 +182,7 @@ public class SteeringMk2 extends SteeringBase {
         double xSum = 0;
 
         // Sum the x's and the y's
-        for (int idx = startTarget; idx <= endTarget; idx++) {
+        for (int idx = startTarget; idx < endTarget; idx++) {
             xSum += midPoints.get(idx).x;
             ySum += midPoints.get(idx).y;
         }
